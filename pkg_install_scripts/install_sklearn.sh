a964ec1333b1d6837a2eceb7c2064030b8036f19
root <root@localhost.localdomain>
2015-01-14 02:41:17 -0500

    apt-get install sklearn

--- pkg_install_scripts/install_sklearn.sh
+++ pkg_install_scripts/install_sklearn.sh
@@ -1,3 +1,4 @@
 echo "Y"|sudo apt-get install -y build-essential python-dev python-setuptools \
                      python-numpy python-scipy \
                      libatlas-dev libatlas3gf-base
+echo "Y"|sudo apt-get install -y python-sklearn
